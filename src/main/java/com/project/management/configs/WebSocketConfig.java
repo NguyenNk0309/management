7f668843ce185715a96212236f9b8300168cf6d2
Khoi Nguyen <khoinguyen.030901@gmail.com>
2023-10-05 19:56:05 +0700

    enable cors

--- src/main/java/com/project/management/configs/WebSocketConfig.java
+++ src/main/java/com/project/management/configs/WebSocketConfig.java
@@ -1,18 +1,12 @@
 package com.project.management.configs;
 
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
-@Configurable
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    private static final String FE_API = "http://localhost:3000";
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -24,20 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
                 .addEndpoint("/ws/registry")
-                .setAllowedOrigins(FE_API)
+                .setAllowedOrigins("*")
                 .withSockJS();
-    }
-
-    @Bean
-    WebMvcConfigurer corsConfig() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*")
-                        .allowedOrigins(FE_API);
-            }
-        };
     }
 }

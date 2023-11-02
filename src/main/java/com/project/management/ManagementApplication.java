6052c8152220fa1e2111f0654e0ff30ae14d7b9b
Khoi Nguyen <khoinguyen.030901@gmail.com>
2023-11-02 23:09:24 +0700

    unChange

--- src/main/java/com/project/management/ManagementApplication.java
+++ src/main/java/com/project/management/ManagementApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ManagementApplication {
 
 	public static void main(String[] args) {
-		System.setProperty("java.net.preferIPv4Stack", "true");
 		SpringApplication.run(ManagementApplication.class, args);
 	}
 

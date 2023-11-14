217ae02d88961cc13b9194731e8ac4797fda5f02
NguyenNk0309 <89288492+NguyenNk0309@users.noreply.github.com>
2023-11-14 09:36:51 +0700

    Update commonConstant.java

--- src/main/java/com/project/management/constants/commonConstant.java
+++ src/main/java/com/project/management/constants/commonConstant.java
@@ -3,5 +3,5 @@ package com.project.management.constants;
 public class commonConstant {
     public static final String LINK_API = "http://localhost:3000";
 
-    public static final String REAL_API = "http://smart-home-management.surge.sh";
+    public static final String REAL_API = "http://luizkun.duckdns.org";
 }
